cd9f8773de229b8fd6356e6c722d0ef55173f809
Konrad Borowski <konrad@borowski.pw>
2018-12-10 07:47:57 +0100

    Reformat code (silly rustfmt)

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,7 @@ use std::io::{self, Read, Write};
 use structopt::StructOpt;
 
 mod day1;
+mod day10;
 mod day2;
 mod day3;
 mod day4;
@@ -11,7 +12,6 @@ mod day6;
 mod day7;
 mod day8;
 mod day9;
-mod day10;
 #[cfg(test)]
 mod testmacros;
 
